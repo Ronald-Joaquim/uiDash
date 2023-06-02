823f13ae962e80a73e41378b2af0698cf46c7b27
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-06-01 23:10:54 -0300

    Instalação e ajustes do chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from "next/app";
-
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
