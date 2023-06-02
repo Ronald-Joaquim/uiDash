27d02f507eabd76d9425aaed14c4cb800d10abfa
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-06-01 22:32:24 -0300

    Criação do novo Projeto de Dashboard com Chakra UI

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h1>Está funcionando</h1>
+  )
+}
