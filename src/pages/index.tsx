7dc67cb70bc5b8b9cb24a8334459872e5abff2b3
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-06-03 23:19:35 -0300

    Criação da tela de SingIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { Inter } from 'next/font/google'
+import { Flex,  Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function SingIn() {
   return (
-    <h1>Está funcionando</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="teal.900"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input type="email" name="email" label="E-mail" />
+
+          <Input type="password" name="password" label="Senha" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
