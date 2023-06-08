5656be4e3c7216f75a8ad6ee23992182849dc25a
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-06-07 23:05:41 -0300

    Header Responsivo

--- src/components/Header/NotificationsNav.tsx
+++ src/components/Header/NotificationsNav.tsx
@@ -3,7 +3,15 @@ import { RiNotificationLine, RiUserAddLine } from "react-icons/ri";
 
 export function NotificationsNav() {
     return (
-        <HStack spacing={8} mx="8" pr="8" py="1" color="teal.300" borderRightWidth={1} borderColor="teal.900">
+        <HStack
+            spacing={["6", "8"]}
+            mx={["6", "8"]}
+            pr={["6", "8"]}
+            py="1"
+            color="teal.300"
+            borderRightWidth={1}
+            borderColor="teal.900"
+        >
             <Icon as={RiNotificationLine} fontSize="20" />
             <Icon as={RiUserAddLine} fontSize="20" />
         </HStack>
--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Ronald Joaquim</Text>
-                <Text color="gray.300" fontSize="small">ronaldjoaquimcardoso11@gmail.com</Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Ronald Joaquim</Text>
+                    <Text color="gray.300" fontSize="small">ronaldjoaquimcardoso11@gmail.com</Text>
+                </Box>
+            )}
+
 
             <Avatar size="md" src="https://github.com/Ronald-Joaquim.png" />
         </Flex>
