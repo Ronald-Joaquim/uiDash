1c16f7a2bae2128724e54c31e69e1e849f803e55
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-06-13 23:15:00 -0300

    Criação do Fake back end

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,12 +4,19 @@ import { Sidebar } from "../../components/Sidebar";
 import { RiAddLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function UserList() {
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
+
+    useEffect(() => {
+            fetch("http://localhost:3000/api/users")
+            .then(response => response.json)
+            .then(data => console.log(data))
+    }, [])
     return (
         <Box>
             <Header />
