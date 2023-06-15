6c443b862a44259e3b01201d739c062f2d0ccd55
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-06-14 22:58:08 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,14 +4,34 @@ import { Sidebar } from "../../components/Sidebar";
 import { RiAddLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
-import { useQuery } from "react-query"
+import { useQuery } from "react-query";
+
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+}
 
 export default function UserList() {
     const { data, isLoading, error } = useQuery("users", async () => {
         const response = await fetch("http://localhost:3000/api/users")
         const data = await response.json()
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric"
+                })
+            }
+        });
+
+        return users;
     })
 
 
@@ -59,55 +79,25 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Ronald Joaquim</Text>
-                                                <Text fontSize="sm" color="gray.300">ronaldjoaquimcardoso11@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>
-                                            05 de junho, 2023
-                                        </Td>}
+                                    {data.map((user: User) => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>
+                                                    {user.createdAt}
+                                                </Td>}
 
-                                    </Tr>
-                                </Tbody>
-                                <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Ronald Joaquim</Text>
-                                                <Text fontSize="sm" color="gray.300">ronaldjoaquimcardoso11@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>
-                                            05 de junho, 2023
-                                        </Td>}
-
-                                    </Tr>
-                                </Tbody>
-                                <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Ronald Joaquim</Text>
-                                                <Text fontSize="sm" color="gray.300">ronaldjoaquimcardoso11@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>
-                                            05 de junho, 2023
-                                        </Td>}
-
-                                    </Tr>
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
                             <Pagination />
